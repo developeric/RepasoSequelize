d4417caba1a6ffb45fca492db1b9a1422ef36319
Eric <patinetakiller564@gmail.com>
2026-03-30 22:44:42 -0300

    Se hicieron todos los modelos, faltan las relaciones

--- src/models/articletag.model.js
+++ src/models/articletag.model.js
@@ -1,1 +1,10 @@
-export const 
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database";
+
+export const ArticleTag = sequelize.define("ArticleTagModel", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+});
--- src/models/index.model.js
+++ src/models/index.model.js
@@ -1,4 +1,27 @@
 import { Article } from "./article.model.js";
+import { ArticleTag } from "./articletag.model.js";
 import { Profile } from "./profile.model.js";
 import { Tag } from "./tag.model.js";
 import { User } from "./user.model.js";
+//Este archivo es un index el cual va a servir para ver las relaciones
+//mucho mas facil sin rellenar de codigo cada archivo.model.js, como si fuera
+//un routes.index.js y asi tener un poco mas ordenado     -Eri del pasado
+/**
+ **INSTALAR EL BETTER COMMENTS
+ */
+//Article
+
+//ArticleTag
+
+//Profile
+
+Profile.belongsTo(User, {
+  foreignKey: "user_id",
+  as: "user",
+  onDelete: "CASCADE",
+});
+
+//Tag
+
+//User
+User.hasOne(Profile, { foreignKey: "user_id" });
